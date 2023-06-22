90b52f8ad42a7bd0b49135550fccf7bf886510e0
Artem-Alexandrov-TestingWEB <artem.alexandrov.testing07101978@gmail.comu>
2023-06-22 14:35:12 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+ 
+    const tabs = document.querySelectorAll('.tabheader__item'),
+                 tabsContent = document.querySelectorAll('.tabcontent'),
+                 tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent () {
+        tabsContent.forEach(item => {
+           item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+           item.classList.remove('tabheader__item_active');
+        });
+    }             
+
+
+
+
+});
